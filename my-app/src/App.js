0bb1cf4a60d4e064f4a98d141d40cd4dcd602c60
cinda87 <j.c.bietz@gmail.com>
2025-03-19 12:15:00 -0600

    added footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,36 @@
-import "./App.css";
+import "./Weather.css";
 import WeatherSearch from "./WeatherSearch";
 
 function App() {
   return (
     <div className="App">
       <WeatherSearch />
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/jacinda-bietz-3158a0338/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Jacinda Bietz
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/CindaCodes/React-Weather-App"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://cinda-codes-weather.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
